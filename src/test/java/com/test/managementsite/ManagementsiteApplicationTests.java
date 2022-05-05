913f055a0f92abfa7da613da09fe4afa78262099
yang <yrzcode@hotmail.com>
2022-05-05 23:06:19 +0900

    Create Project

---
+++ src/test/java/com/test/managementsite/ManagementsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.managementsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
