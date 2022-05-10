68dc0273661f1a7f9dae907606e76452020ddfac
yang <yrzcode@hotmail.com>
2022-05-10 18:36:22 +0900

    Initial 2.1

--- src/main/java/com/test/managementsite/controller/newmember/AddNewMember.java
+++ src/main/java/com/test/managementsite/controller/newmember/AddNewMember.java
@@ -7,7 +7,6 @@ import org.springframework.validation.BindingResult;
 import com.test.managementsite.data.MemberForm;
 import com.test.managementsite.service.IMemberManager;
 
-import lombok.var;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
