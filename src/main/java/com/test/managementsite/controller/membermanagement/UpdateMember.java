00fad6f8c7a3caacdb11f7682a7be88563b5e24b
yang <yrzcode@hotmail.com>
2022-05-11 15:40:21 +0900

    # Style management page
    * add update button

---
+++ src/main/java/com/test/managementsite/controller/membermanagement/UpdateMember.java
@@ -0,0 +1,20 @@
+package com.test.managementsite.controller.membermanagement;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UpdateMember {
+
+	@GetMapping("update-member")
+	public String getSearchMember(){
+		return "member-management";
+	}
+	
+	@PostMapping("update-member")
+	public String postSearchMember(){
+		return "member-management";
+	}
+	
+}
---
+++ src/main/java/com/test/managementsite/data/SearchMemberForm.java
@@ -0,0 +1,12 @@
+package com.test.managementsite.data;
+
+public record SearchMemberForm (
+		
+	String name,
+	String sex,
+	String age
+
+		) {
+}
+
+
